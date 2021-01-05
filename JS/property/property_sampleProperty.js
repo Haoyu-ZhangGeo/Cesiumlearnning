15406bc924a1055ad2627fb7c6c1016ddf85e46a
SURVING-MAPPING <58218215+SURVING-MAPPING@users.noreply.github.com>
2021-01-05 17:36:35 +0800

    1.5
    property 机制更新

--- JS/property/property_sampleProperty.js
+++ JS/property/property_sampleProperty.js
@@ -9,10 +9,24 @@ const mapBox = new Cesium.MapboxImageryProvider({
     accessToken:'pk.eyJ1Ijoic3VydmluZy1tYXBwaW5nIiwiYSI6ImNraWJmempiODBoMWcycmw2N3N4dHJsemcifQ.377MGhK03RK1Vlsc_Ktt4Q'
     
 });
+//时间设置为东八区
+//const start = viewer.clock.currentTime = Cesium.JulianDate.addHours(Cesium.JulianDate.now(new Date()), 8, new Cesium.JulianDate());
+//自定义时间系统
+const clock = new Cesium.Clock({
+    startTime: Cesium.JulianDate.fromIso8601("2021-01-05"),
+    currentTime: Cesium.JulianDate.fromIso8601("2021-01-05"),
+    stopTime: Cesium.JulianDate.fromIso8601("2021-01-06"),
+    //开启时间循环
+    clockRange: Cesium.ClockRange.LOOP_STOP, 
+    clockStep: Cesium.ClockStep.SYSTEM_CLOCK_MULTIPLIER,
+    multiplier: 4000, //
+    shouldAnimate: true, // 
+  });
+
 // 引出视窗，去除屏幕下方IU控件
 const viewer = new Cesium.Viewer('cesiumContainer', {
     imageryProvider:mapBox,
-   animation:false,//左下角时间动画视窗
+   animation:true,//左下角时间动画视窗
    baseLayerPicker:true,//右上角底图选择按钮
    geocoder:false,//搜索按钮
   // homeButton:false,//回到默认视角按钮
@@ -20,10 +34,11 @@ const viewer = new Cesium.Viewer('cesiumContainer', {
    timeline:false,//下方时间轴
    fullscreenButton:false,//视窗全屏按钮
    shouldAnimate : true,//开启模型动画效果
-   
+   clockViewModel: new Cesium.ClockViewModel(clock),
  });
 //    取消双击事件
 viewer.cesiumWidget.screenSpaceEventHandler.removeInputAction(Cesium.ScreenSpaceEventType.LEFT_DOUBLE_CLICK);
+
 
 //创建盒子
 const greenBox = viewer.entities.add({
@@ -37,5 +52,11 @@ const greenBox = viewer.entities.add({
 
 });
 
+//设置samplepropert，使盒子随时间变化
 const property = new Cesium.SampledProperty(Cesium.Cartesian3);
-    property.addSample(Cesium.JulianDate.from);
+      property.addSample(Cesium.JulianDate.fromIso8601('2021-01-05T00:00:00.00Z'),new Cesium.Cartesian3(400000,300000,100000));
+      property.addSample(Cesium.JulianDate.fromIso8601('2021-01-06T00:00:00.00Z'),new Cesium.Cartesian3(400000,300000,1000000));
+
+greenBox.box.dimensions = property;
+
+      
